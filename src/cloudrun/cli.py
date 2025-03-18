57c1fa623a32b1a925e70773c9f152ed2eea8402
Jon Clegg <jon.clegg@paramount.com>
2025-03-18 00:39:34 -0500

    .

--- src/cloudrun/cli.py
+++ src/cloudrun/cli.py
@@ -4,28 +4,30 @@ import boto3
 import sys
 
 @click.group()
-def cli():
+@click.option('--profile', help='AWS profile to use')
+@click.pass_context
+def cli(ctx, profile):
     """CloudRun CLI tools"""
-    pass
+    ctx.ensure_object(dict)
+    ctx.obj['profile'] = profile
+    
+    # Configure AWS session with profile if provided
+    if profile:
+        session = boto3.Session(profile_name=profile)
+        # Set credentials for boto3 default session
+        credentials = session.get_credentials()
+        os.environ['AWS_ACCESS_KEY_ID'] = credentials.access_key
+        os.environ['AWS_SECRET_ACCESS_KEY'] = credentials.secret_key
+        if session.region_name:
+            os.environ['AWS_DEFAULT_REGION'] = session.region_name
 
 @cli.command()
 @click.option('--region', help='AWS region to use')
-@click.option('--profile', help='AWS profile to use')
-def setup(region, profile):
+def setup(region):
     """Initialize AWS infrastructure for CloudRun"""
     try:
         # Import here to avoid circular imports
         from .setup import create_infrastructure
-        
-        # Configure AWS session with profile if provided
-        if profile:
-            session = boto3.Session(profile_name=profile)
-            # Set credentials for boto3 default session
-            credentials = session.get_credentials()
-            os.environ['AWS_ACCESS_KEY_ID'] = credentials.access_key
-            os.environ['AWS_SECRET_ACCESS_KEY'] = credentials.secret_key
-            if session.region_name:
-                os.environ['AWS_DEFAULT_REGION'] = session.region_name
         
         # Create infrastructure
         resources = create_infrastructure(region)
@@ -40,7 +42,7 @@ def setup(region, profile):
         raise
 
 def main():
-    cli(sys.argv[1:])
+    cli(obj={})
 
 if __name__ == '__main__':
     main() 
